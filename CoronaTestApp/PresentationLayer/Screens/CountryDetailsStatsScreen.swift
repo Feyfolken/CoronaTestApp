1bb60cc9c0fe9ad846bd05287eeb7838bd44b8de
Vadim Krasilnikov <vkrasilnikov@diasoft.ru>
2021-04-08 11:10:27 +0300

    CurrentDateView added on CountryDetailsStatsScren.

--- CoronaTestApp/PresentationLayer/Screens/CountryDetailsStatsScreen.swift
+++ CoronaTestApp/PresentationLayer/Screens/CountryDetailsStatsScreen.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct CountryDetailsStatsScreen: View {
     @AppStorage ("isDarkMode") private var isDarkMode = false
-    
     @State var countryCovidStats: CountryCovidStatistic
     
     var body: some View {
@@ -22,6 +21,8 @@ struct CountryDetailsStatsScreen: View {
                 .edgesIgnoringSafeArea(.bottom)
             
             ScrollView {
+                Spacer()
+                CurrentDateView()
                 CountryDetailsStatsPanelView(countryCovidStats: $countryCovidStats)
             }
         }.navigationTitle("\(countryCovidStats.country)")
@@ -32,6 +33,31 @@ struct CountryDetailsStatsScreen_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             CountryDetailsStatsScreen(countryCovidStats: CovidStatisticsService.mockCovidStatsData.countries.first!)
+        }
+    }
+}
+
+struct CurrentDateView: View {
+    @State var shadowRadius: CGFloat = 3
+
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 42, style: .continuous)
+                .frame(width: 300.0, height: 108.0)
+                .foregroundColor(Color("StatsPanelColor"))
+                .shadow(color: Color("InfoViewDarkerColor") , radius: shadowRadius)
+                .onAppear(perform: {
+                    let baseAnimation = Animation.easeOut(duration: 0.7)
+                    let repeated = baseAnimation.repeatForever(autoreverses: true)
+                    
+                    return withAnimation(repeated) {
+                        shadowRadius = 10
+                    }
+                })
+            
+            Text(Date(), style: .date)
+                .font(.custom("Kefa", size: 28))
+                .foregroundColor(Color("InfoViewDarkerColor"))
         }
     }
 }
